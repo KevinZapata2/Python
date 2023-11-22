9d1e65c17d23a547634c822aee6826b34c988e9d
TamaLord <camilopetro12@gmail.com>
2023-11-22 14:29:54 -0500

    first commit

---
+++ variable.py
@@ -0,0 +1,23 @@
+a = 45 
+b = 1.2
+c= 45 
+d = "hola"
+print(type(d))
+if a==c:
+    print("a y c son iguales")
+else:
+    print("a y b no son")
+
+#DIVISION
+Division = 20//5
+print(Division)    
+#PONTECIACION
+pontencia = 2**3
+print(pontencia)
+potencia1 =pow(3,3)
+print(potencia1)
+#RESIDUO: % (Modulo)
+resto = 20%8
+print(resto)
+
+    
